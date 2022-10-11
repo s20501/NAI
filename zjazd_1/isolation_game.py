da7c1f49a654133126bf26a5ab1e879076f733ed
s20501 <s20501@pjwstk.edu.pl>
2022-10-11 16:31:45 +0200

    Player position refactor

--- zjazd_1/isolation_game.py
+++ zjazd_1/isolation_game.py
@@ -8,8 +8,8 @@ class IsolationGame(TwoPlayerGame):
         self.players = players
         self.board = [[]]
         self.current_player = 1
-        self.player1_position = [0, 2]
-        self.player2_position = [7, 2]
+        self.players_position = [[0, 2], [7, 3]]
+        self.current_field = "O"
 
         # fill board
         for x in range(8):
@@ -18,24 +18,36 @@ class IsolationGame(TwoPlayerGame):
                 self.board[x].append([])
                 self.board[x][y] = "O"
 
-    def possible_moves(self): return ['w', 'a', 's', 'd']
+    def possible_moves(self): return ["w", "a", "s", "d"]
 
-    def make_move(self, move): self.pile -= int(move)  # remove bones.
+    def make_move(self, move):
+        match move:
+            case "w":
+                pass
+            case "a":
+                pass
+            case "s":
+                pass
+            case "d":
+                pass
 
-    def win(self): return False
+    def win(self): self.current_field == "X"
 
     def is_over(self): return self.win()  # Game stops when someone wins.
 
+    # Draw the board
     def show(self):
         for y in range(6):
             print()
             for x in range(8):
-                if x == self.player1_position[0] and y == self.player1_position[1]:
-                    print(Fore.BLUE + 'I', end="")
-                elif x == self.player2_position[0] and y == self.player2_position[1]:
-                    print(Fore.RED + 'I', end="")
+                if x == self.players_position[0][0] and y == self.players_position[0][1]:
+                    print(Fore.BLUE + "I", end="")
+                elif x == self.players_position[1][0] and y == self.players_position[1][1]:
+                    print(Fore.RED + "I", end="")
                 else:
                     print(Fore.WHITE + self.board[x][y], end="")
+
+        print()
 
     def scoring(self): return 100 if self.win() else 0  # For the AI
 
