a7b81a7fb2abe29ab07e3e97d94e9b21bc967db2
s20501 <s20501@pjwstk.edu.pl>
2022-11-03 15:05:51 +0100

    Names refactor

--- zjazd_2/main.py
+++ zjazd_2/main.py
@@ -4,24 +4,26 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
 temperature = ctrl.Antecedent(np.arange(0, 100, 1), 'temperature')
-ice_cubes = ctrl.Consequent(np.arange(0, 11, 1), 'ice-cubes')
+cube_size = ctrl.Antecedent(np.arange(0, 4, 1), 'cube_size')
+glass_size = ctrl.Antecedent(np.arange(0, 4, 1), 'glass_size')
+cube_count = ctrl.Consequent(np.arange(0, 11, 1), 'cube_count')
 
-ice_cubes['low'] = fuzz.trimf(ice_cubes.universe, [0, 0, 2])
-ice_cubes['medium'] = fuzz.trimf(ice_cubes.universe, [0, 2, 4])
-ice_cubes['high'] = fuzz.trimf(ice_cubes.universe, [4, 8, 10])
+cube_count['low'] = fuzz.trimf(cube_count.universe, [0, 0, 2])
+cube_count['medium'] = fuzz.trimf(cube_count.universe, [2, 3, 4])
+cube_count['high'] = fuzz.trimf(cube_count.universe, [4, 8, 10])
 
 temperature.automf(3, 'quant', ['cold', 'mild', 'hot'])
 
 
-rule1 = ctrl.Rule(temperature['cold'], ice_cubes['low'])
-rule2 = ctrl.Rule(temperature['mild'], ice_cubes['medium'])
-rule3 = ctrl.Rule(temperature['hot'], ice_cubes['high'])
+rule1 = ctrl.Rule(temperature['cold'], cube_count['low'])
+rule2 = ctrl.Rule(temperature['mild'], cube_count['medium'])
+rule3 = ctrl.Rule(temperature['hot'], cube_count['high'])
 
 temp_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
 temp = ctrl.ControlSystemSimulation(temp_ctrl)
 
-temp.input['temperature'] = int(input("Temperature: "))
+temp.input['temperature'] = int(input("Drink temperature(C): "))
 
 temp.compute()
 
-print(temp.output['ice-cubes'])
+print(temp.output['cube_count'])
