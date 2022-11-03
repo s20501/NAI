6e0596326e530ea032ba988d04f8e0d327e40356
s20501 <s20501@pjwstk.edu.pl>
2022-11-03 16:06:07 +0100

    Docs

--- zjazd_2/main.py
+++ zjazd_2/main.py
@@ -1,12 +1,23 @@
+# Authors: Marcin Żmuda-Trzebiatowski and Jakub Cirocki
+# Example: ...
+#
+# The program calculates the count of cubes needed to chill the drink.
+# To run program install
+# pip install scikit-fuzzy
+
+
 import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
+# New Antecedent/Consequent objects hold universe variables and membership functions
+# definitions of temperature, cube size, glass size and cube count
 temperature = ctrl.Antecedent(np.arange(0, 101, 1), 'temperature')
 cube_size = ctrl.Antecedent(np.arange(1, 4, 1), 'cube_size')
 glass_size = ctrl.Antecedent(np.arange(1, 4, 1), 'glass_size')
 cube_count = ctrl.Consequent(np.arange(0, 11, 1), 'cube_count')
 
+# Costume-membership functions
 cube_count['low'] = fuzz.trimf(cube_count.universe, [0, 0, 2])
 cube_count['medium'] = fuzz.trimf(cube_count.universe, [2, 4, 4])
 cube_count['high'] = fuzz.trimf(cube_count.universe, [4, 10, 10])
@@ -15,9 +26,11 @@ temperature['cold'] = fuzz.trimf(temperature.universe, [0, 0, 15])
 temperature['mild'] = fuzz.trimf(temperature.universe, [15, 40, 40])
 temperature['hot'] = fuzz.trimf(temperature.universe, [40, 100, 100])
 
+# Auto-membership functions
 glass_size.automf(3, 'quant', ['small', 'medium', 'big'])
 cube_size.automf(3, 'quant', ['small', 'medium', 'big'])
 
+# Rules. You need fewer cubes when the cube size is bigger and the glass is smaller
 rules = [
     ctrl.Rule(
         temperature['cold'] & (glass_size['small'] | glass_size['medium'] | cube_size['medium'] | cube_size['big']),
@@ -28,13 +41,17 @@ rules = [
         cube_count['high'])
 ]
 
+# Simulating control system
 temp_ctrl = ctrl.ControlSystem(rules)
 temp = ctrl.ControlSystemSimulation(temp_ctrl)
 
+# Getting inputs
 temp.input['temperature'] = int(input("Drink temperature(C): "))
 temp.input['cube_size'] = int(input("Ice cube size(1-3): "))
 temp.input['glass_size'] = int(input("Glass size(1-3): "))
 
+# Compute
 temp.compute()
 
+# Printing outcome
 print(temp.output['cube_count'])
