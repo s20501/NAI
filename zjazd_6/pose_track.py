a6759614c2c8662f75d70ae6d4a3211c6f391f9d
MrCosik <cirocki.jakub1@gmail.com>
2023-01-12 19:30:06 +0100

    Added function descriptions

--- zjazd_6/pose_track.py
+++ zjazd_6/pose_track.py
@@ -1,18 +1,21 @@
 import cv2
 import numpy as np
 import os
-from matplotlib import pyplot as plt
-import time
 import mediapipe as mp
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from tensorflow.keras.callbacks import TensorBoard
 
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 def mediapipe_detection(image, model):
+    """
+    This function returns mediapipe model prediction and current frame
+    Args:
+        image: current frame
+        model: mediapipe model
+    """
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
     image.flags.writeable = False                  # Image is no longer writeable
     results = model.process(image)                 # Make prediction
@@ -20,13 +23,13 @@ def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
     return image, results
 
-def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
-
 def draw_styled_landmarks(image, results):
+    """
+    This function draws landmark lines on frame of given footage
+    Args:
+        image:  one frame camera input
+        results: mediapipe model predictions for body parts
+    """
     # Draw face connections
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
@@ -48,7 +51,16 @@ def draw_styled_landmarks(image, results):
                              mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                              )
 
+
 def extract_keypoints(results):
+    '''
+    This function extracts body keypoints and returns them.
+    Args:
+        results:     The input image of person(s) whose facial landmarks needs to be detected.
+    Returns:
+        output_image: A copy of input image with face landmarks drawn.
+        results:      The output of the facial landmarks detection on the input image.
+    '''
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
@@ -70,63 +82,57 @@ sequence_length = 30
 
 # Folder start
 start_folder = 1
-#
-# for action in actions:
-#     # dirmax = np.max(np.array(os.listdir(os.path.join(DATA_PATH, action))).astype(int))
-#     for sequence in range(no_sequences):
-#         try:
-#             os.makedirs(os.path.join(DATA_PATH, action, str(sequence+1)))
-#         except:
-#             pass
+
+for action in actions:
+    for sequence in range(no_sequences):
+        try:
+            os.makedirs(os.path.join(DATA_PATH, action, str(sequence+1)))
+        except:
+            pass
 
 
-# cap = cv2.VideoCapture(0)
-# # Set mediapipe model
-# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-#     # NEW LOOP
-#     # Loop through actions
-#     for action in actions:
-#         # Loop through sequences aka videos
-#         for sequence in range(start_folder, start_folder + no_sequences):
-#             # Loop through video length aka sequence length
-#             for frame_num in range(sequence_length):
-#
-#                 # Read feed
-#                 ret, frame = cap.read()
-#
-#                 # Make detections
-#                 image, results = mediapipe_detection(frame, holistic)
-#
-#                 # Draw landmarks
-#                 draw_styled_landmarks(image, results)
-#
-#                 # NEW Apply wait logic
-#                 # if frame_num == 0:
-#                 #     cv2.putText(image, 'STARTING COLLECTION', (120, 200),
-#                 #                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
-#                 #     cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
-#                 #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-#                 #     # Show to screen
-#                 #     cv2.imshow('OpenCV Feed', image)
-#                 #     cv2.waitKey(500)
-#                 # else:
-#                 #     cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
-#                 #                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-#                 #     # Show to screen
-#
-#                 cv2.imshow('OpenCV Feed', image)
-#
-#                 # # NEW Export keypoints
-#                 # keypoints = extract_keypoints(results)
-#                 # npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
-#                 # np.save(npy_path, keypoints)
-#
-#                 # Break gracefully
-#                 if cv2.waitKey(10) & 0xFF == ord('q'):
-#                     break
-#
-#     cap.release()
-#     cv2.destroyAllWindows()
+cap = cv2.VideoCapture(0)
+# Set mediapipe model
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    # Loop through actions
+    for action in actions:
+        # Loop through sequences aka videos
+        for sequence in range(start_folder, start_folder + no_sequences):
+            # Loop through video length aka sequence length
+            for frame_num in range(sequence_length):
+
+                # Read feed
+                ret, frame = cap.read()
+
+                # Make detections
+                image, results = mediapipe_detection(frame, holistic)
+
+                # Draw landmarks
+                draw_styled_landmarks(image, results)
+
+                if frame_num == 0:
+                    cv2.putText(image, 'STARTING COLLECTION', (120, 200),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 4, cv2.LINE_AA)
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+                    # Show to screen
+                    cv2.imshow('OpenCV Feed', image)
+                    cv2.waitKey(500)
+                else:
+                    cv2.putText(image, 'Collecting frames for {} Video Number {}'.format(action, sequence), (15, 12),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+                cv2.imshow('OpenCV Feed', image)
+
+                keypoints = extract_keypoints(results)
+                npy_path = os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+
+                if cv2.waitKey(10) & 0xFF == ord('q'):
+                    break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 label_map = {label:num for num, label in enumerate(actions)}
@@ -145,8 +151,7 @@ X = np.array(sequences)
 y = to_categorical(labels).astype(int)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
 
-# log_dir = os.path.join('Logs')
-# tb_callback = TensorBoard(log_dir=log_dir)
+
 
 model = Sequential()
 model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
@@ -161,5 +166,4 @@ model.fit(X_train, y_train, epochs=500)
 
 model.save('action.h5')
 
-# print(model.summary())
 
